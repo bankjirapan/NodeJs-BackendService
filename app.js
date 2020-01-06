46163ae47073d44b91f4f1cbf03568279d47d14c
llUJO <jirapan_yankhan@hotmail.com>
2020-01-07 00:17:42 +0700

    [ EDIT ] Fixed bug

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const bodyPaser = require('body-parser');
 const path = require('path');
 const shopRouter = require('./routes/shop');
 const adminRouter = require('./routes/admin');
+const rootDir = require('./util/path');
 
 const app = express();
 
-app.use(shopRouter);
-app.use(adminRouter);
-
-
 app.use(bodyPaser.urlencoded({extended: false}))
+app.use(shopRouter);
+app.use('/admin',adminRouter.routes);
+
+
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req,res) =>{
-  res.sendStatus(404).sendFile(path.join(__dirname,'../','views','404.html'));
+  res.status(404).sendFile(path.join(rootDir,'views','404.html'));
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const  path = require('path');
+const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir,'views','add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res) => {
-  console.log(req.body.productName);
+router.post('/add-product', (req, res) => {
+  products.push({
+    title: req.body.title
+  });
+
   res.redirect('/');
 });
 
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
