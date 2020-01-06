56e4f37e5c9c8deff14d460d4560554a8d87420d
llUJO <jirapan_yankhan@hotmail.com>
2020-01-06 22:17:10 +0700

    [ EDIT ] EDIT link to admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+router.use('/admin/add-product', (req, res, next) => {
   res.send(
     '<form action="/product" method="post"> <input type="text" name="productName"> <button type="submit">Add</button> </form>'
   );
 });
 
-router.post('/product', (req, res) => {
+router.post('/admin/product', (req, res) => {
   console.log(req.body.productName);
   res.redirect('/');
 });
